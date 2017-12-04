4e8a88272dccd324c4dd3e173b18315f06bc34dd
Karolina Strzelecka <kstrzelecka0594@gmail.com>
2017-12-04 12:51:06 +0100

    Add triagle script

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// js script
+
+var a = prompt('Enter base');
+var h = prompt('Enter height');
+var triangleArea;
+
+triangleArea =  a* h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
